a9eb7d08f1a083b7a5a15ad303ad76d1c13701ce
sanjus-robotic-studio <58104863+sanjus-robotic-studio@users.noreply.github.com>
2021-04-17 14:50:16 +0530

    init

---
+++ pages/api/hello.ts
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { connectToDatabase } from "./_connector";
+
+export default async (req, res) => {
+  await connectToDatabase();
+
+  res.status(200).json({ name: 'John Doe' })
+}
